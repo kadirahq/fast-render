7d639e144c9fcd9b57374a9a3976a572f0162dca
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-12-22 14:47:05 +0530

    added better special handling cleaning support

--- client/ddp_update.js
+++ client/ddp_update.js
@@ -24,12 +24,32 @@ Meteor.default_connection._send = function(msg) {
   setTimeout(function() {
     if(msg.msg == 'sub' && FastRender._subscriptions && FastRender._subscriptions[msg.name]) {
       console.log('fake ready sending');
-      self._livedata_data({msg:"ready",subs:[msg.id]});
-      //we don't need to handle specially after this
-      delete FastRender._subscriptions[msg.name];
+      self._livedata_data({msg:"ready",subs:[msg.id], frGen: true});
+      FastRender._subscriptionIdMap[msg.id] = msg.name;
     }
   }, 0);
+
   return originalSend.call(this, msg);
+};
+
+/*
+  This will take care of cleaning special subscription handling after the actual subscription comes out
+*/
+var originalReady = Meteor.default_connection._process_ready;
+Meteor.default_connection._process_ready = function(msg) {
+  if(msg.msg == 'ready' && !msg.frGen && FastRender._subscriptions) {
+    msg.subs.forEach(function(subId) {
+      var subscription = FastRender._subscriptionIdMap[subId];
+      if(subscription) {
+        console.log('deleting subscription', subscription, subId);
+        //we don't need to handle specially after this
+        delete FastRender._subscriptions[msg.name];
+        delete FastRender._ironRouterSubscriptions[msg.name];
+        delete FastRender._subscriptionIdMap[subId];
+      }
+    });
+  }
+  return originalReady.call(this, msg);
 };
 
 /*
@@ -39,7 +59,6 @@ var originalSubscribe = Meteor.subscribe;
 Meteor.subscribe = function(subscription) {
   var rtn = originalSubscribe.apply(this, arguments);
   if(FastRender._ironRouterSubscriptions && FastRender._ironRouterSubscriptions[subscription]) {
-    delete FastRender._ironRouterSubscriptions[subscription];
     return undefined;
   } else {
     return rtn;
