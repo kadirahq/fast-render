b618ce4b9cfefeda6ddb9b6e819f708b9e2eedf9
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-12-22 19:40:53 +0530

    simplied ddp alterations

--- client/ddp_update.js
+++ client/ddp_update.js
@@ -1,22 +1,36 @@
-//we are inserting docs to a collection manually
-//but when the data comes from the subscription, it will also try to insert
-//but since there are some exiting data, meteor throws an execption
-//here comes the fix --- blahh.....
-
-var original = Meteor.default_connection.constructor.prototype.registerStore;
-Meteor.default_connection.constructor.prototype.registerStore = function(name, store) {
-  var originalUpdate = store.update;
-  store.update = function(msg) {
-    if(msg.msg == 'added') {
-      var localCollection = Meteor.default_connection._mongo_livedata_collections[name];
-      if(localCollection.findOne(msg.id)) {
-        msg.msg = 'changed';
-      }
+var originalLivedataData = Meteor.default_connection._livedata_data;
+Meteor.default_connection._livedata_data = function(msg) {
+  console.log('MSG', msg);
+  
+  //we are inserting docs to a collection manually
+  //but when the data comes from the subscription, it will also try to insert
+  //but since there are some exiting data, meteor throws an execption
+  //here comes the fix
+  if(msg.msg == 'added') {
+    var localCollection = Meteor.default_connection._mongo_livedata_collections[msg.collection];
+    if(localCollection.findOne(msg.id)) {
+      msg.msg = 'changed';
     }
-    return originalUpdate.call(this, msg);
   }
-  return original.call(this, name, store);
-}
+
+
+  //This will take care of cleaning special subscription handling 
+  //after the actual subscription comes out
+  if(msg.msg == 'ready' && !msg.frGen && FastRender._subscriptions) {
+    msg.subs.forEach(function(subId) {
+      var subscription = FastRender._subscriptionIdMap[subId];
+      if(subscription) {
+        console.log('deleting subscription', subscription, subId);
+        //we don't need to handle specially after this
+        delete FastRender._subscriptions[subscription];
+        delete FastRender._ironRouterSubscriptions[subscription];
+        delete FastRender._subscriptionIdMap[subId];
+      }
+    });
+  }
+
+  originalLivedataData.call(this, msg);
+};
 
 var originalSend = Meteor.default_connection._send;
 Meteor.default_connection._send = function(msg) {
@@ -30,26 +44,6 @@ Meteor.default_connection._send = function(msg) {
   }, 0);
 
   return originalSend.call(this, msg);
-};
-
-/*
-  This will take care of cleaning special subscription handling after the actual subscription comes out
-*/
-var originalReady = Meteor.default_connection._process_ready;
-Meteor.default_connection._process_ready = function(msg) {
-  if(msg.msg == 'ready' && !msg.frGen && FastRender._subscriptions) {
-    msg.subs.forEach(function(subId) {
-      var subscription = FastRender._subscriptionIdMap[subId];
-      if(subscription) {
-        console.log('deleting subscription', subscription, subId);
-        //we don't need to handle specially after this
-        delete FastRender._subscriptions[subscription];
-        delete FastRender._ironRouterSubscriptions[subscription];
-        delete FastRender._subscriptionIdMap[subId];
-      }
-    });
-  }
-  return originalReady.call(this, msg);
 };
 
 /*
