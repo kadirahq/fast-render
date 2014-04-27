cc09d1c3d82b4a2f40e0f7b03f5603d09cec86a7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-04-27 18:50:50 +0530

    remove fast-render in the test folder from git

--- test/packages/fast-render/lib/utils.js
+++
@@ -1,9 +0,0 @@
-EncodeEJSON = function(ejson) {
-  var ejsonString = EJSON.stringify(ejson);
-  return encodeURI(ejsonString);
-};
-
-DecodeEJSON = function(encodedEjson) {
-  var decodedEjsonString = decodeURI(encodedEjson);
-  return EJSON.fromJSONValue(JSON.parse(decodedEjsonString));
-};
