4e3b5b8f7d9c51b50bc97fac44cbce115f71ea08
Dominik Guzei <dominik.guzei@gmail.com>
2014-06-11 10:54:42 +0200

    fixes iron-router errors while using tinytest
    The changes add support for standalone packages that are not contained within a Meteor application.

--- package.js
+++ package.js
@@ -16,14 +16,18 @@ Package.on_use(function(api) {
   // This is needed due to Meteor Issue #1358
   //   https://github.com/meteor/meteor/issues/1358
   // The 'weak' flag doesn't support non-core (ie. atmosphere) packages yet.
-  if(isIronRouterExists()) {
-    api.use(['iron-router'], ['client', 'server'], {weak: true});
+  if(isMeteorAppWithIronRouterDependency() || isPackageWithIronRouterDependency()) {
+
+    // the app or package uses iron-router -> so we can use it too!
+    api.use(['iron-router'], ['client', 'server']);
   }
 
-  //a hack to detect if IR has been added or removed from the app
-  //  if IR was not there on the app and added later, FR cannot detect it,
-  //  since Meteor caches packages. this is how we force FR to invalidate the cache.
-  api.add_files('../../.meteor/packages', ['client', 'server']);
+  if(isMeteorAppWithIronRouterDependency()) {
+    //a hack to detect if IR has been added or removed from the app
+    //  if IR was not there on the app and added later, FR cannot detect it,
+    //  since Meteor caches packages. this is how we force FR to invalidate the cache.
+    api.add_files('../../.meteor/packages', ['client', 'server']);
+  }
 
   api.add_files([
     'lib/server/inject_data.html',
@@ -66,6 +70,14 @@ function isAppDir(filepath) {
   }
 }
 
+function isPackageDirectory(filepath) {
+  try {
+    return fs.statSync(path.join(filepath, 'package.js')).isFile();
+  } catch (e) {
+    return false;
+  }
+}
+
 function meteorRoot() {
   var currentDir = process.cwd();
   while (currentDir) {
@@ -83,13 +95,27 @@ function meteorRoot() {
   return currentDir;
 }
 
-function isIronRouterExists() {
+function isMeteorAppWithIronRouterDependency() {
   try {
     var meteorPackages = fs.readFileSync(path.join(meteorRoot(), '.meteor', 'packages'), 'utf8');
     return !!meteorPackages.match(/iron-router/);
   } catch(ex) {
     // seems like FastRender running outside a Meteor app (ie: with tinytest)
     // So there is no iron-router
+    return false;
+  }
+}
+
+function isPackageWithIronRouterDependency() {
+
+  var processDirectory = process.cwd();
+
+  if(isPackageDirectory(processDirectory)) {
+
+    var packageFile = fs.readFileSync(path.join(processDirectory, 'package.js'), 'utf8');
+    return !!packageFile.match(/iron-router/);
+  }
+  else {
     return false;
   }
 }
