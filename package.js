1c7e55563cca394d54e0461b01ecd3214c5071c0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-11-21 06:51:03 +0530

    test utils

--- package.js
+++ package.js
@@ -13,6 +13,21 @@ Npm.depends({
 });
 
 Package.on_use(function(api) {
+  configure(api);
+  api.export('FastRender', ['client', 'server']);
+  api.export('__init_fast_render', ['client']);
+});
+
+Package.onTest(function(api) {
+  configure(api);
+  api.use('tinytest', ['client', 'server']);
+
+  api.addFiles([
+    'tests/utils.js'
+  ], ['client', 'server']);
+});
+
+function configure (api) {
   api.versionsFrom('METEOR@0.9.3');
   api.use('iron:router@0.9.0 || 1.0.0', ['client', 'server'], {weak: true});
   api.use('chuangbo:cookie@1.1.0', 'client');
@@ -20,15 +35,15 @@ Package.on_use(function(api) {
   api.use(['minimongo', 'livedata', 'ejson', 'underscore', 'webapp', 'routepolicy', 'accounts-base'], ['server']);
   api.use(['minimongo', 'underscore', 'deps', 'ejson', 'accounts-base'], ['client']);
 
-  api.add_files([
+  api.addFiles([
     'lib/server/inject_data.html',
   ], 'server', {isAsset: true});
 
-  api.add_files([
+  api.addFiles([
     'lib/utils.js'
   ], ['client', 'server']);
 
-  api.add_files([
+  api.addFiles([
     'lib/server/utils.js',
     'lib/server/fast_render.js',
     'lib/server/publish_context.js',
@@ -37,7 +52,7 @@ Package.on_use(function(api) {
     'lib/server/iron_router_support.js',
   ], 'server');
 
-  api.add_files([
+  api.addFiles([
     'lib/client/log.js',
     'lib/client/fast_render.js',
     'lib/client/ddp_update.js',
@@ -45,6 +60,4 @@ Package.on_use(function(api) {
     'lib/client/auth.js'
   ], 'client');
 
-  api.export('FastRender', ['client', 'server']);
-  api.export('__init_fast_render', ['client']);
-});
+}
