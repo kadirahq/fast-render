b4ec1b5d334be2238de83ecdaeb7eb40075ed608
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-21 21:40:58 +0530

    version bumped

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var path = Npm.require('path');
 
 Package.describe({
   "summary": "Render you app even before the DDP connection comes live. - magic?",
-  "version": "2.2.0",
+  "version": "2.3.0",
   "git": "https://github.com/meteorhacks/fast-render",
   "name": "meteorhacks:fast-render"
 });
