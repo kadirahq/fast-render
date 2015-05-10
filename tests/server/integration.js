9203dcb9baa0790d9da3311c677e496c54e25d58
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-05-10 22:13:39 +0530

    Only run fast-render for HTML pages

--- tests/server/integration.js
+++ tests/server/integration.js
@@ -190,8 +190,12 @@ Tinytest.add('integration - error inside a null publication', function(test) {
 var urlResolve = Npm.require('url').resolve;
 function getFRData(path) {
   var url = urlResolve(process.env.ROOT_URL, path);
-  var res = HTTP.get(url);
-
+  var options = {
+    headers: {
+      "Accept": "text/html"
+    }
+  };
+  var res = HTTP.get(url, options);
 
   var encodedData = res.content.match(/data">(.*)<\/script/)[1];
   return InjectData._decode(encodedData)['fast-render-data'];
